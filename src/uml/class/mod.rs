f34cb0562536fbf157ac723fab3443692d9715ed
usagrada <usagrada.kind@gmail.com>
2021-12-27 02:08:22 +0900

    class図も変更

--- src/uml/class/mod.rs
+++ src/uml/class/mod.rs
@@ -1,8 +1,9 @@
+mod node;
 use crate::helper::*;
 use crate::theme::{Theme, ThemeName};
 use crate::MakeSvg;
-use svg::node::element::{Group, Line, Rectangle, Text};
-use svg::node::Text as TextNode;
+use node::*;
+use svg::node::element::Group;
 use svg::Document;
 const FONT_SIZE: usize = 8;
 const PADDING: usize = 3;
@@ -25,11 +26,11 @@ impl ClassUML {
   }
 
   pub fn add_class(&mut self, name: &str, elements: &[(bool, &str)], methods: &[(bool, &str)]) {
-    self.nodes.push(ClassNode {
-      name: name.to_string(),
-      elements: elements.iter().map(|&e| Element::new(e)).collect(),
-      methods: methods.iter().map(|&m| Method::new(m)).collect(),
-    });
+    self.nodes.push(ClassNode::new(
+      name,
+      elements.iter().map(|&e| ClassElement::new(e)).collect(),
+      methods.iter().map(|&m| ClassMethod::new(m)).collect(),
+    ));
   }
 }
 
@@ -56,183 +57,5 @@ impl MakeSvg for ClassUML {
 fn class_test() {
   let mut class = ClassUML::new("class component");
   class.add_class("hello world", &[], &[]);
-  assert!(
-    class.nodes
-      == vec![ClassNode {
-        name: String::from("hello world"),
-        elements: vec![],
-        methods: vec![],
-      }]
-  );
-}
-
-#[allow(dead_code)]
-#[derive(PartialEq)]
-struct ClassNode {
-  name: String,
-  elements: Vec<Element>,
-  methods: Vec<Method>,
-}
-
-impl ClassNode {
-  fn make_svg(&self, theme: &Theme) -> Group {
-    let node_rect = make_rect(100, 100, theme);
-    let node_text = self.make_text();
-    let elements_svg = self.make_elements_svg();
-    let methods_svg = self.make_methods_svg();
-
-    let height_title = FONT_SIZE * 2;
-    let line1 = Line::new()
-      .set("x1", 0)
-      .set("y1", height_title)
-      .set("x2", 100)
-      .set("y2", height_title)
-      .set("stroke", "#000");
-    let elements_height = (FONT_SIZE + PADDING) * self.elements.len();
-
-    let line2 = Line::new()
-      .set("x1", 0)
-      .set("y1", height_title + elements_height + MARGIN * 2)
-      .set("x2", 100)
-      .set("y2", height_title + elements_height + MARGIN * 2)
-      .set("stroke", "#000");
-
-    Group::new()
-      .add(node_rect)
-      .add(line1)
-      .add(line2)
-      .add(node_text)
-      .add(elements_svg)
-      .add(methods_svg)
-  }
-
-  fn make_text(&self) -> Text {
-    let text_node = TextNode::new(self.name.clone());
-    Text::new()
-      .add(text_node)
-      .set("x", 50) // width / 2
-      .set("y", 3 * FONT_SIZE / 2)
-      .set("font-size", FONT_SIZE * 3 / 2)
-      .set("text-anchor", "middle")
-  }
-
-  fn make_elements_svg(&self) -> Group {
-    let mut group = Group::new().set("transform", format!("translate(0, {})", 2 * FONT_SIZE));
-    let elements_svg: Vec<Group> = self
-      .elements
-      .iter()
-      .enumerate()
-      .map(|(index, el)| {
-        el.make_svg().set(
-          "transform",
-          format!(
-            "translate(10, {})",
-            MARGIN + FONT_SIZE + index * (FONT_SIZE + PADDING)
-          ),
-        )
-      })
-      .collect();
-    for element in elements_svg {
-      group = group.add(element);
-    }
-    group
-  }
-
-  /// make group of methods
-  fn make_methods_svg(&self) -> Group {
-    let mut group = Group::new().set(
-      "transform",
-      format!(
-        "translate(0,{})",
-        2 * FONT_SIZE + (FONT_SIZE + PADDING) * self.elements.len() + MARGIN * 2
-      ),
-    );
-    let methods_svg = self.methods.iter().enumerate().map(|(index, method)| {
-      method.make_svg().set(
-        "transform",
-        format!(
-          "translate(10, {})",
-          MARGIN + FONT_SIZE + index * (FONT_SIZE + PADDING)
-        ),
-      )
-    });
-    for method in methods_svg {
-      group = group.add(method);
-    }
-    group
-  }
-}
-
-fn make_rect(width: usize, height: usize, theme: &Theme) -> Rectangle {
-  (width, height).make_rect().set_theme(theme)
-}
-
-#[derive(PartialEq)]
-struct Element {
-  public: bool,
-  name: String,
-}
-
-impl Element {
-  fn new<T: Into<String>>(e: (bool, T)) -> Self {
-    Self {
-      public: e.0,
-      name: e.1.into(),
-    }
-  }
-
-  fn make_svg(&self) -> Group {
-    Group::new().add(self.make_mark()).add(self.make_text())
-  }
-
-  fn make_text(&self) -> Text {
-    let text_node = TextNode::new(self.name.clone());
-    Text::new()
-      .add(text_node)
-      .set("x", FONT_SIZE)
-      .set("font-size", FONT_SIZE)
-  }
-  fn make_mark(&self) -> Text {
-    let mark = if self.public { "+" } else { "-" };
-    let mark_node = TextNode::new(mark);
-    Text::new()
-      .add(mark_node)
-      .set("text-anchor", "middle")
-      .set("font-size", FONT_SIZE)
-  }
-}
-
-impl Method {
-  fn new<T: Into<String>>(m: (bool, T)) -> Self {
-    Self {
-      public: m.0,
-      name: m.1.into(),
-    }
-  }
-
-  fn make_svg(&self) -> Group {
-    Group::new().add(self.make_mark()).add(self.make_text())
-  }
-
-  fn make_text(&self) -> Text {
-    let text_node = TextNode::new(self.name.clone());
-    Text::new()
-      .add(text_node)
-      .set("x", FONT_SIZE)
-      .set("font-size", FONT_SIZE)
-  }
-  fn make_mark(&self) -> Text {
-    let mark = if self.public { "+" } else { "-" };
-    let mark_node = TextNode::new(mark);
-    Text::new()
-      .add(mark_node)
-      .set("text-anchor", "middle")
-      .set("font-size", FONT_SIZE)
-  }
-}
-
-#[derive(PartialEq)]
-struct Method {
-  public: bool,
-  name: String,
+  assert!(class.nodes == vec![ClassNode::new("hello world", vec![], vec![])]);
 }
--- src/uml/sequence/mod.rs
+++ src/uml/sequence/mod.rs
@@ -1,7 +1,7 @@
 mod edge;
 mod node;
-mod sequence;
-pub use sequence::Sequence;
+use edge::Edge;
+use node::Node;
 
 const RECT_HEIGHT: usize = 20;
 const FONT_SIZE: usize = 8;
@@ -31,4 +31,205 @@ fn position_x(index: usize, max_length: usize) -> usize {
 #[inline]
 fn position_y(_index: usize, _max_length: usize) -> usize {
   Y_INDEX
+}
+
+use crate::{
+  helper::*,
+  make_vec,
+  theme::{Theme, ThemeName},
+  MakeSvg,
+};
+use std::collections::{HashMap, HashSet};
+use svg::node::element::Group;
+use svg::Document;
+
+pub struct Sequence {
+  nodes: HashMap<Node, usize>,
+  edges: Vec<Edge>,
+  max_length: usize,
+  markers: HashSet<Markers>,
+  theme: Theme,
+}
+
+impl Sequence {
+  pub fn new(theme: ThemeName) -> Self {
+    Sequence {
+      nodes: HashMap::new(),
+      edges: Vec::new(),
+      markers: HashSet::new(),
+      max_length: 0,
+      theme: Theme::new(theme),
+    }
+  }
+
+  pub fn add_node(&mut self, text: &str) -> &Self {
+    self.nodes.insert(
+      Node {
+        name: text.to_string(),
+      },
+      self.nodes.len(),
+    );
+    if self.max_length < text.len() {
+      self.max_length = text.len();
+    }
+    self
+  }
+  pub fn add_nodes(&mut self, texts: Vec<&str>) -> &Self {
+    for text in texts {
+      self.add_node(text);
+    }
+    self
+  }
+
+  pub fn add_edge(&mut self, edge: (&str, &str, &str, Markers)) -> &Self {
+    let (start, end, text, marker) = edge;
+    self.markers.insert(marker.clone());
+    let source_index = self.nodes.get(&Node { name: start.into() });
+    let target_index = self.nodes.get(&Node { name: end.into() });
+
+    match (source_index, target_index) {
+      (Some(&s), Some(&t)) => self.edges.push(Edge::new(s, t, text.to_string(), marker)),
+      (_, _) => println!("invalid error"),
+    }
+    self
+  }
+
+  pub fn add_edges(&mut self, edges: Vec<(&str, &str, &str, Markers)>) -> &Self {
+    for edge in edges {
+      self.add_edge(edge);
+    }
+    self
+  }
+
+  fn make_nodes(&self) -> Vec<Group> {
+    let rect_width = rect_width(self.max_length);
+    let vertical_height = self.get_vertical_height();
+    self
+      .nodes
+      .iter()
+      .map(|(node, &index)| {
+        let (x, y) = position(index, self.max_length);
+        let option = make_vec![
+          ("fill", self.theme.color.rect.text),
+          ("text-anchor", "middle"),
+          ("dominant-baseline", "central"),
+          ("font-size", FONT_SIZE)
+        ];
+        let text_element1 = node
+          .make_svg()
+          .position(x + rect_width / 2, y + RECT_HEIGHT / 2)
+          .set_values(&option);
+
+        let text_element2 = node
+          .make_svg()
+          .position(x + rect_width / 2, y + vertical_height + RECT_HEIGHT / 2)
+          .set_values(&option);
+
+        let rect_element1 = (rect_width, RECT_HEIGHT)
+          .make_rect()
+          .position(x, y)
+          .set("rx", 2usize)
+          .set("ry", 2usize)
+          .set("fill", self.theme.color.rect.fill)
+          .set("stroke", self.theme.color.rect.frame)
+          .set("stroke-width", 1usize);
+        let rect_element2 = rect_element1.clone().set("y", y + vertical_height);
+
+        Group::new()
+          .add(rect_element1)
+          .add(text_element1)
+          .add(rect_element2)
+          .add(text_element2)
+      })
+      .collect()
+  }
+
+  fn get_vertical_height(&self) -> usize {
+    std::cmp::max(DEFAULT_HEIGHT, (self.edges.len() + 1) * VERTICAL_HEIGHT)
+  }
+
+  // 縦線を引く
+  fn make_vertical_lines(&self) -> Vec<Group> {
+    let height = self.get_vertical_height() - RECT_HEIGHT;
+    self
+      .nodes
+      .iter()
+      .enumerate()
+      .map(|(index, _)| {
+        let (mut x, mut y) = position(index, self.max_length);
+        x += rect_width(self.max_length) / 2;
+        y += RECT_HEIGHT;
+        let path = (x, y, x, y + height)
+          .make_line()
+          // .set("stroke-dasharray", "4")
+          .set("stroke", self.theme.color.line.second);
+        println!("{}", self.theme.color.line.second);
+        Group::new().add(path)
+      })
+      .collect()
+  }
+
+  // 横線を引く
+  fn make_horizontal_lines(&self) -> Vec<Group> {
+    self
+      .edges
+      .iter()
+      .enumerate()
+      .map(|(index, value)| {
+        let mut x1 = value.position_node1(self.max_length).0;
+        let mut x2 = value.position_node2(self.max_length).0;
+        let (_, y) = position(index, self.max_length);
+        let y_path = y + RECT_HEIGHT / 2 + VERTICAL_HEIGHT * (index + 1);
+        x1 += rect_width(self.max_length) >> 1;
+        x2 += rect_width(self.max_length) >> 1;
+        let path = (x1, y_path, x2, y_path)
+          .make_line()
+          .set("stroke", self.theme.color.line.primary)
+          .add_marker_end(&value.marker_type());
+        let x_mid = (x1 + x2) >> 1;
+        let x = x_mid;
+        let y = y_path - FONT_SIZE;
+        let text_element = value
+          .make_text()
+          .position(x, y)
+          .set("text-anchor", "middle")
+          .set("fill", self.theme.color.text_primary)
+          .set("font-size", FONT_SIZE);
+        Group::new().add(path).add(text_element)
+      })
+      .collect()
+  }
+}
+
+impl MakeSvg for Sequence {
+  fn make_svg(&self) -> Document {
+    let mut sequence_group = Group::new();
+    for vline in self.make_vertical_lines() {
+      sequence_group = sequence_group.add(vline);
+    }
+    for node in self.make_nodes() {
+      sequence_group = sequence_group.add(node);
+    }
+    for hline in self.make_horizontal_lines() {
+      sequence_group = sequence_group.add(hline);
+    }
+    use svg::node::element::Definitions;
+    let mut defs = Definitions::new();
+    for markers in self.markers.iter() {
+      let marker_svg = markers.make_svg();
+      defs = defs.add(marker_svg);
+    }
+
+    Document::new()
+      .set("viewBox", self.bounding_box())
+      .add(defs)
+      .add(sequence_group)
+  }
+
+  fn bounding_box(&self) -> (usize, usize, usize, usize) {
+    let x =
+      2 * X_INDEX + (self.max_length * FONT_SIZE + PADDING * 2 + MARGIN * 2) * self.nodes.len();
+    let y = 2 * Y_INDEX + RECT_HEIGHT * 2 + self.get_vertical_height();
+    (0, 0, x, y)
+  }
 }
